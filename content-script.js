d9bfe082e933b0e2ce8ce1820db6826931d72feb
Zgao <gaozhipeng@starcross.cn>
2021-01-27 11:58:32 +0800

    添加代码

---
+++ content-script.js
@@ -0,0 +1,47 @@
+﻿function Func() {
+	return new Promise((resolve, reject) => {
+		var jsString = document.querySelector("#player >script:nth-child(2)")
+		if (!jsString) {
+			jsString = document.querySelector("#player >script:nth-child(1)")
+		}
+		jsString = jsString.innerHTML
+		jsString = `	var playerObjList = {};\n${jsString}`
+		var flashvars = jsString.match("flashvars_[0-9]{1,}")[0]
+		eval(jsString)
+		var jsObject = eval(flashvars)
+		resolve(jsObject)
+	})
+}
+
+Func().then(res => {
+	var videoType = []
+	Object.keys(res['mediaDefinitions']).forEach((item) => {
+		var itemInfo = res['mediaDefinitions'][item]
+		if (itemInfo['format'] == 'mp4') {
+			var mp4UrlJson = getMp4Url(itemInfo['videoUrl']);
+			for (const mp4Url of mp4UrlJson){
+				var obj = {
+				key: mp4Url['quality'],
+				val: mp4Url['videoUrl'],
+				video_title:res.video_title
+				}
+			videoType.push(obj)
+			}
+		}
+	})
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.cmd == 'test') 
+			sendResponse(videoType);
+	});
+})
+
+function getMp4Url(url){
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url , false);
+	xhr.onload = function(){
+	if (xhr.readyState === 4 && xhr.status === 200){
+		mp4UrlJson = JSON.parse(xhr.responseText);
+	}};
+	xhr.send();
+	return mp4UrlJson;
+}
---
+++ popup.js
@@ -0,0 +1,50 @@
+﻿function sendMessageToContentScript(message, callback) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+            if (callback) callback(response);
+        });
+    });
+}
+sendMessageToContentScript({ cmd: 'test', value: 'test' }, function (videoType) {
+    if(videoType == null) return 
+    var boxEl = document.getElementsByTagName('ul')[0]
+    var videoStr = ''
+    videoType.forEach((item) => {
+        videoStr += `<li> <label>清晰度：<span> ${item.key} </span> </label> <button class="button down">下载</button> <button class="button copy">复制</button></li>`
+    });
+    boxEl.innerHTML = videoStr
+    var dialog = document.getElementsByTagName("dialog")[0]
+    var dialog1 = document.getElementsByTagName("dialog")[1]
+    var downList = document.querySelectorAll(".down")
+    downList.forEach((item, index) => {
+        item.onclick = () => {
+            let reg =  /[\~\.\:\/\*\?\"\|\\\<\>]/g
+            chrome.downloads.download({
+                url: videoType[index].val,
+                filename: videoType[index].video_title.replace(reg, '') + ".mp4"
+            })
+            dialog1.showModal();
+            setTimeout(() => {
+                dialog1.close();
+            }, 2000)
+        }
+    })
+
+    var copyList = document.querySelectorAll(".copy")
+    copyList.forEach((item, index) => {
+        item.onclick = () => {
+            var url = videoType[index].val
+            var oInput = document.createElement('input');
+            oInput.value = url;
+            document.body.appendChild(oInput);
+            oInput.select();
+            document.execCommand("Copy");
+            oInput.className = 'oInput';
+            oInput.style.display = 'none';
+            dialog.showModal();
+            setTimeout(() => {
+                dialog.close();
+            }, 1500)
+        }
+    })
+});
